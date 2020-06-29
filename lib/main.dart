ca2969a7b036ab053552c0f161581a9ba8693d0b
Andrea <andreaelhage@hotmail.com>
2020-06-29 18:32:09 -0400

    Added navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,81 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'package:event_app/screens/home_screen.dart';
+import 'package:event_app/screens/map.dart';
+import 'package:event_app/screens/notifications.dart';
+import 'package:event_app/screens/offers.dart';
+import 'package:event_app/screens/safety.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  int _selectedPage = 2;
+  final _pageOptions = [
+    Notifications(),
+    Map(),
+    HomeScreen(),
+    Offers(),
+    Safety(),
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Event App',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.deepPurple[600],
+      ),
       home: Scaffold(
-        body: Center(
-          child: RandomWords(),
+        body: _pageOptions[_selectedPage],
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.deepPurple[600],
+          currentIndex: _selectedPage,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+          iconSize: 30.0,
+          onTap: (int index) {
+            setState(() {
+              _selectedPage = index;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.notifications,
+              ),
+              title: SizedBox.shrink(),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.place,
+              ),
+              title: SizedBox.shrink(),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+              ),
+              title: SizedBox.shrink(),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.shopping_cart,
+              ),
+              title: SizedBox.shrink(),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.security,
+              ),
+              title: SizedBox.shrink(),
+            ),
+          ],
         ),
       ),
     );
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  @override
-  Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
   }
 }
---
+++ lib/models/activity.dart
@@ -0,0 +1,21 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class Activity {
+  String destination;
+  var time;
+  String date;
+  String description;
+  var location;
+  var icon;
+
+  Activity({
+    this.destination,
+    this.time,
+    this.date,
+    this.description,
+    this.location,
+    this.icon,
+  });
+}
