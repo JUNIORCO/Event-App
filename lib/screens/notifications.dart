c2ad9df59749b207194a4d9150d9142c484fcca1
Sami Junior <sami.kahil@mail.mcgill.ca>
2020-07-01 17:17:18 -0400

    Notification kinda works

--- lib/screens/notifications.dart
+++ lib/screens/notifications.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/notification_card.dart';
+import 'package:intl/intl.dart';
 
 class Notifications extends StatelessWidget {
   final List<String> entries = <String>[
@@ -7,14 +8,12 @@ class Notifications extends StatelessWidget {
     'BEACH CLUB DAY! LETS GOOOOOOOOOOO This one will be slightly shorter but nonethless a few sentences long!!! Amazing',
     'Welcome Froshies! Have a wonderful time.',
     'LETS GO FOR A RIDE BABYYYYYY',
-    'WOWWWWWWWWWWWW'
-  ];
-  final List<Color> colors = <Color>[
-    Colors.white,
-    Colors.white,
-    Colors.white,
-    Colors.white,
-    Colors.white
+    'WOWWWWWWWWWWWW',
+    'Buses leave in 1 hour. Location: John Molson Gym! Im going to make this text really really long and the next one really really short to see if the sizing is working. If its working ill be really happy. if not ill cry',
+    'BEACH CLUB DAY! LETS GOOOOOOOOOOO This one will be slightly shorter but nonethless a few sentences long!!! Amazing',
+    'Welcome Froshies! Have a wonderful time.',
+    'LETS GO FOR A RIDE BABYYYYYY',
+    'WOWWWWWWWWWWWW',
   ];
   final List<AssetImage> images = <AssetImage>[
     AssetImage('assets/images/frosh.jpg'),
@@ -22,25 +21,102 @@ class Notifications extends StatelessWidget {
     AssetImage('assets/images/frosh.jpg'),
     AssetImage('assets/images/uni_logo.png'),
     AssetImage('assets/images/frosh.jpg'),
+    AssetImage('assets/images/frosh.jpg'),
+    AssetImage('assets/images/uni_logo.png'),
+    AssetImage('assets/images/frosh.jpg'),
+    AssetImage('assets/images/uni_logo.png'),
+    AssetImage('assets/images/frosh.jpg'),
   ];
+  final List<DateTime> dates = <DateTime>[
+    DateTime.utc(2020, DateTime.july, 1, 11, 30),
+    DateTime.utc(2020, DateTime.july, 1, 6, 21),
+    DateTime.utc(2020, DateTime.june, 30, 14, 59),
+    DateTime.utc(2020, DateTime.june, 30, 9, 0),
+    DateTime.utc(2020, DateTime.june, 29, 20, 10),
+    DateTime.utc(2020, DateTime.june, 29, 15, 44),
+    DateTime.utc(2020, DateTime.june, 29, 4, 44),
+    DateTime.utc(2020, DateTime.june, 28, 12, 00),
+    DateTime.utc(2020, DateTime.june, 28, 23, 59),
+    DateTime.utc(2020, DateTime.june, 27, 8, 36),
+  ];
+
+  Divider buildDivider(int index) {
+    return const Divider(
+      color: Color(0x00000000), // transparent color
+      height: 0.0,
+      endIndent: 0.0,
+      indent: 0.0,
+      thickness: 0.0,
+    );
+  }
+
+  Widget buildItem(int index) {
+    String formattedToday = DateFormat('MMMMd').format(DateTime.now());
+    String formattedDate = DateFormat('MMMMd').format(dates[index]);
+    String formattedPrev = (index == 0)
+        ? DateFormat('MMMMd').format(dates[index])
+        : DateFormat('MMMMd').format(dates[index - 1]);
+
+    // determine opacity of notification card
+    double opacity = (formattedToday == formattedDate) ? 1.0 : 0.5;
+
+    // if first notification card, header is Today
+    // else if the current and previous notification cards are identical, header is empty
+    // else header is the date of the notification card
+    if (index == 0) {
+      return NotificationCard(
+          entries[index], images[index], dates[index], opacity, 'Today');
+    } else if (formattedDate == formattedPrev) {
+      return NotificationCard(
+          entries[index], images[index], dates[index], opacity, '');
+    } else {
+      return NotificationCard(
+          entries[index], images[index], dates[index], opacity, formattedDate);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Notifications'),
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.pink[300],
+            Colors.deepPurple[600],
+          ],
         ),
-        body: ListView.separated(
-          padding: const EdgeInsets.all(8),
-          itemCount: entries.length,
-          itemBuilder: (BuildContext context, int index) {
-            return NotificationCard(
-                colors[index], entries[index], images[index]);
-          },
-          separatorBuilder: (BuildContext context, int index) => const Divider(
-            color: Color(0x00000000), // transparent color
-            height: 0.0,
+      ),
+      child: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "Notifications",
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                  fontSize: 25.0,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white),
+            ),
+            margin: EdgeInsets.only(
+                top: 60.0, right: 20.0, bottom: 20.0, left: 20.0),
           ),
-        ));
+          Expanded(
+            child: ListView.separated(
+              shrinkWrap: true,
+              scrollDirection: Axis.vertical,
+              padding: const EdgeInsets.all(8),
+              itemCount: entries.length,
+              itemBuilder: (BuildContext context, int index) =>
+                  buildItem(index),
+              separatorBuilder: (BuildContext context, int index) =>
+                  buildDivider(index),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/widgets/notification_card.dart
+++ lib/widgets/notification_card.dart
@@ -1,44 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class NotificationCard extends StatelessWidget {
   // Underscore before the variable name means it's a private variable
   // final: once you set it, you can't change it
-  final Color _color;
   final String _text;
-  final AssetImage _image;
+  AssetImage _image;
+  final DateTime _date;
+  final DateFormat formatter = new DateFormat('MMMMd');
+  double _opacity;
+  String _header;
 
   // Constructor
-  // NotificationDetail(Color color) {
-  //   this._color = color;
-  // }
-  // the shorthand version is
-  NotificationCard(this._color, this._text, this._image);
+  NotificationCard(
+      this._text, this._image, this._date, this._opacity, this._header);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        decoration: BoxDecoration(
-          color: _color,
-          borderRadius: BorderRadius.circular(25),
-        ),
-        margin: const EdgeInsets.all(20.0),
-        padding: const EdgeInsets.all(20.0),
-        child: Row(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(right: 10.0),
-              child: CircleAvatar(
-                backgroundImage: _image,
-                radius: 30,
-              ),
+    return Column(
+      // crossAxisAlignment: CrossAxisAlignment.start,
+      // mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Visibility(
+          visible: !(_header.length == 0),
+          child: Container(
+            alignment: Alignment.centerLeft,
+            margin: EdgeInsets.only(
+                top: 20.0, right: 20.0, bottom: 20.0, left: 20.0),
+            child: Text(
+              _header,
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white.withOpacity(_opacity)),
             ),
-            Expanded(
-              child: Text(
-                _text,
-                textAlign: TextAlign.left,
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(_opacity),
+            borderRadius: BorderRadius.circular(30.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.3),
+                spreadRadius: 5,
+                offset: Offset(0, 3),
+                blurRadius: 7.0,
               ),
-            )
-          ],
-        ));
+            ],
+          ),
+          margin: const EdgeInsets.only(bottom: 20.0, left: 20.0, right: 20.0),
+          padding: const EdgeInsets.all(20.0),
+          child: Row(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(right: 10.0),
+                child: CircleAvatar(
+                  backgroundImage: _image,
+                  radius: 30,
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  _text,
+                  textAlign: TextAlign.left,
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Container(
+  //     decoration: BoxDecoration(
+  //       color: Colors.white.withOpacity(_opacity),
+  //       borderRadius: BorderRadius.circular(30.0),
+  //       boxShadow: [
+  //         BoxShadow(
+  //           color: Colors.grey.withOpacity(0.3),
+  //           spreadRadius: 5,
+  //           offset: Offset(0, 3),
+  //           blurRadius: 7.0,
+  //         ),
+  //       ],
+  //     ),
+  //     margin: const EdgeInsets.only(bottom: 20.0, left: 20.0, right: 20.0),
+  //     padding: const EdgeInsets.all(20.0),
+  //     child: Row(
+  //       children: <Widget>[
+  //         Padding(
+  //           padding: EdgeInsets.only(right: 10.0),
+  //           child: CircleAvatar(
+  //             backgroundImage: _image,
+  //             radius: 30,
+  //           ),
+  //         ),
+  //         Expanded(
+  //           child: Text(
+  //             _text,
+  //             textAlign: TextAlign.left,
+  //           ),
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
 }
