56547229d702e9c87073176f5b338a152d810394
Sami Junior <sami.kahil@mail.mcgill.ca>
2020-07-04 12:59:32 -0400

    Maps

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -2,51 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map/plugin_api.dart';
 import 'package:latlong/latlong.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class Map extends StatefulWidget {
-  @override
-  _MapState createState() => _MapState();
-}
-
-class _MapState extends State<Map> {
+class Map extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new FlutterMap(
-        options: new MapOptions(
-          center: new LatLng(34.056340, -118.232050),
-          zoom: 13.0,
-        ),
-        layers: [
-          new TileLayerOptions(
-            urlTemplate: "https://api.magbox.com/v2/"
-                "{id}/{z}/{x}/{y}@2x.png?access_token={accessToken}",
-            additionalOptions: {
-              'accessToken':
-                  'pk.eyJ1IjoiYW5kcmVhZWxoYWdlIiwiYSI6ImNrYzVnaGQ3OTA1MGgyeXFleWYxcXpvZG4ifQ.4koN6CUxcjXXGoughVaGWw',
-              'id': 'ckc5ghd79050h2yqeyf1qzodn',
-            },
-          ),
-          MarkerLayerOptions(
-            markers: [
-              Marker(
-                width: 80,
-                height: 80,
-                point: LatLng(34.056340, -118.232050),
-                builder: (ctx) => Container(
-                  child: IconButton(
-                    icon: Icon(FontAwesomeIcons.mapMarkerAlt),
-                    color: Color(0xFF6200ee),
-                    iconSize: 45.0,
-                    onPressed: () {},
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ],
+    return new FlutterMap(
+      options: new MapOptions(
+        center: new LatLng(51.5, -0.09),
+        zoom: 13.0,
       ),
+      layers: [
+        new TileLayerOptions(
+          urlTemplate:
+              "https://api.mapbox.com/styles/v1/andreaelhage/ckc7v5qgo0kh61inuw7kg3hvl/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYW5kcmVhZWxoYWdlIiwiYSI6ImNrYzViZ2czbDAxNnIycG4wdHJsZ2xlNmMifQ.qqdl78eo8WH2Vs7-ZyeI_w",
+          additionalOptions: {
+            'accessToken':
+                'pk.eyJ1IjoiYW5kcmVhZWxoYWdlIiwiYSI6ImNrYzViZ2czbDAxNnIycG4wdHJsZ2xlNmMifQ.qqdl78eo8WH2Vs7-ZyeI_w',
+            'id': 'mapbox.streets',
+          },
+        ),
+        new MarkerLayerOptions(
+          markers: [
+            new Marker(
+              width: 80.0,
+              height: 80.0,
+              point: new LatLng(51.5, -0.09),
+              builder: (ctx) => new Container(
+                child: new FlutterLogo(),
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
